a9bb088011ea85ab092066032b11f48278182679
FirdausyTama <atama.firdausy@gmail.com>
2025-06-06 19:51:02 +0700

    perbaikan auth repository

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:canary_app/data/model/request/auth/login_request_model.dart';
 import 'package:canary_app/data/model/request/auth/register_request_model.dart';
+import 'package:canary_app/data/model/response/auth_response_model.dart';
 import 'package:canary_app/services/service_http_client.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:dartz/dartz.dart';
 
 class AuthRepository {
   final ServiceHttpClient _serviceHttpClient;
@@ -9,7 +14,57 @@ class AuthRepository {
 
   AuthRepository(this._serviceHttpClient);
 
-  Future login(LoginRequestModel requestModel) async {}
+  Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "login",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "authToken",
+          value: loginResponse.user!.token,
+        );
+        await secureStorage.write(
+          key: "userRole",
+          value: loginResponse.user!.role,
+        );
+        log("Login successful: ${loginResponse.message}");
+        return Right(loginResponse);
+      } else {
+        log("Login failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Login failed");
+      }
+    } catch (e) {
+      log("Error in login: $e");
+      return Left("An error occurred while logging in.");
+    }
+  }
 
-  Future register(RegisterRequestModel requestModel) async {}
+  Future<Either<String, String>> register(
+    RegisterRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "register",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final registerResponse = jsonResponse['message'] as String;
+        log("Registration successful: ${registerResponse}");
+        return Right(registerResponse);
+      } else {
+        log("Registration failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Registration failed");
+      }
+    } catch (e) {
+      log("Error in registration: $e");
+      return Left("An error occurred while registering.");
+    }
+  }
 }
