ea1972a9c8c1c7fe3b5b44e3d5f609e107796468
FirdausyTama <atama.firdausy@gmail.com>
2025-06-06 10:49:15 +0700

    memuat future login dan register

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,3 +1,5 @@
+import 'package:canary_app/data/model/request/auth/login_request_model.dart';
+import 'package:canary_app/data/model/request/auth/register_request_model.dart';
 import 'package:canary_app/services/service_http_client.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -6,4 +8,8 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
+
+  Future login(LoginRequestModel requestModel) async {}
+
+  Future register(RegisterRequestModel requestModel) async {}
 }
